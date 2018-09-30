e7881473862e46cfb00eaf15cd55db589df8494c
Irene Ong <okirynn@gmail.com>
2018-09-30 17:31:25 -0400

    Modified file

--- src/testarray.java
+++ src/testarray.java
@@ -2,15 +2,15 @@ import java.util.*;
 public class testarray {
 
 	public static void main(String[] args) {
-		ArrayList listA = new ArrayList();
-		ArrayList listB = new ArrayList(15);
+		ArrayList listA = new ArrayList(); //Testing public ArrayList()
+		ArrayList listB = new ArrayList(15); //Testing public ArrayList(int n)
 		Random rand = new Random();
 		
 		System.out.println("The lists BEFORE adding elements:");
 		System.out.print("This is ListA: ");
 		listA.display();
-		System.out.println("ListA has "+ listA.size()+" elements.");
-		
+		System.out.println("ListA has "+ listA.size()+" elements."); //Testing public int size();
+
 		System.out.print("This is ListB: ");
 		listB.display();
 		System.out.println("ListB has "+ listB.size()+" elements.");
@@ -18,7 +18,7 @@ public class testarray {
 		
 		for(int i = 0; i < 15; i++) {
 			int num = rand.nextInt((25 - 1) + 1) + 1;
-			listA.add(num);
+			listA.add(num); //Testing public void add(Object x);
 			listB.add(num);
 		}
 		
